0b9a549d1bbb42525fe35ca96d1104fa5c4a5378
Samir <Saitwalsamir@gmail.com>
2021-12-03 19:13:23 +0530

    --Author added

--- Creed_Backend/Home/models.py
+++ Creed_Backend/Home/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.conf import settings
 
 class Category(models.Model):
     name = models.CharField(max_length=200, blank=False, null=True)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Author', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -17,6 +18,7 @@ class Group_Data(models.Model):
     group_name = models.CharField(max_length=300, null=False, blank=False)
     total_members = models.IntegerField(null=True, blank=True)
     category = models.ForeignKey(Category, blank=False, null=False, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Author', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.group_name
@@ -26,6 +28,7 @@ class About_us(models.Model):
     text = models.TextField(null=False, blank=False)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Author', on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.created_at)
