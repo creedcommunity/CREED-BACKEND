572c42966d648857043e10286923d90d5a3e3c45
Samir <Saitwalsamir@gmail.com>
2021-12-08 19:14:32 +0530

    --Comment section added

--- Creed_Backend/Blog/admin.py
+++ Creed_Backend/Blog/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import *
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['category', 'author', 'tital']
+    search_fields = ['category']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['text', 'name', 'post', 'created_at']
+    search_fields = ['name']
+
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Comment, CommentAdmin)
--- Creed_Backend/Blog/models.py
+++ Creed_Backend/Blog/models.py
@@ -6,6 +6,7 @@ from django.conf import settings
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Author', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey("Blog.Category", on_delete=models.CASCADE)
     updated_at = models.DateTimeField(auto_now_add=True)
     text = models.TextField(blank=False, null=False)
     like = models.IntegerField(default=0)
@@ -15,3 +16,25 @@ class Post(models.Model):
 
     def __str__(self):
         return self.tital
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Author', on_delete=models.CASCADE,
+                               related_name='Author')
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False)
+    text = models.CharField(max_length=400, blank=False, null=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
